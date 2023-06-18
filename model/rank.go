09ab6e015b5e37c9df3a4960e866f0a43609af06
HoangDuck <hoanghuuduc22@gmail.com>
2023-06-18 17:09:34 +0700

    fix: delete duration fields rank

--- model/rank.go
+++ model/rank.go
@@ -12,16 +12,16 @@ type Rank struct {
 }
 
 type UserRank struct {
-	ID            string    `json:"id" gorm:"primary_key"`
-	UserId        string    `json:"user_id"`
-	User          *User     `json:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	RankId        string    `json:"rank_id"`
-	Rank          Rank      `json:"rank" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	BeginAt       time.Time `json:"begin_at" gorm:"begin_at"`
-	CreatedAt     time.Time `json:"created_at" gorm:"created_at"`
-	UpdatedAt     time.Time `json:"updated_at" gorm:"updated_at"`
-	ExpiredAt     time.Time `json:"expired_at" gorm:"expired_at"`
-	DurationYear  int       `json:"duration_year" gorm:"duration_year"`
-	DurationMonth int       `json:"duration_month" gorm:"duration_month"`
-	DurationDay   int       `json:"duration_day" gorm:"duration_day"`
+	ID        string    `json:"id" gorm:"primary_key"`
+	UserId    string    `json:"user_id"`
+	User      *User     `json:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	RankId    string    `json:"rank_id"`
+	Rank      Rank      `json:"rank" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	BeginAt   time.Time `json:"begin_at" gorm:"begin_at"`
+	CreatedAt time.Time `json:"created_at" gorm:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"updated_at"`
+	ExpiredAt time.Time `json:"expired_at" gorm:"expired_at"`
+	//DurationYear  int       `json:"duration_year" gorm:"duration_year"`
+	//DurationMonth int       `json:"duration_month" gorm:"duration_month"`
+	//DurationDay   int       `json:"duration_day" gorm:"duration_day"`
 }
