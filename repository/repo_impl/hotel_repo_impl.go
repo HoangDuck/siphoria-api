f528802bfac1bf2502f6c461487851aee224326a
HoangDuck <hoanghuuduc22@gmail.com>
2023-07-17 20:30:24 +0700

    fix: api get hotel by partner

--- repository/repo_impl/hotel_repo_impl.go
+++ repository/repo_impl/hotel_repo_impl.go
@@ -249,7 +249,7 @@ func (hotelReceiver *HotelRepoImpl) GetHotelFilter(queryModel *query.DataQueryMo
 	var listHotel []model.Hotel
 	//err :=
 	err := GenerateQueryGetData(hotelReceiver.sql, queryModel, &model.Hotel{}, queryModel.ListIgnoreColumns)
-	err = err.Where("id in (Select hotel_id from hotel_works where user_id = ?)", queryModel.UserId)
+	err = err.Where("(id in (Select hotel_id from hotel_works where user_id = ?) OR hotelier_id = ?)", queryModel.UserId, queryModel.UserId)
 	var countTotalRows int64
 	err.Model(model.Hotel{}).Count(&countTotalRows)
 	queryModel.TotalRows = int(countTotalRows)
