88526e9c86ea63ad59bde5a1ba67ee5179ca6192
HoangDuck <hoanghuuduc22@gmail.com>
2023-07-18 10:28:05 +0700

    fix: apply voucher

--- repository/repo_impl/payment_repo_impl.go
+++ repository/repo_impl/payment_repo_impl.go
@@ -95,7 +95,7 @@ func (paymentReceiver *PaymentRepoImpl) UpdateUserRank(userRank model.UserRank) 
 }
 
 func (paymentReceiver *PaymentRepoImpl) ApplyVoucherPayments(requestApplyVoucher req.RequestApplyVoucher) (bool, error) {
-	err := paymentReceiver.sql.Db.Exec("call sp_applyvoucher_v2(?,?:text);",
+	err := paymentReceiver.sql.Db.Exec("call sp_applyvoucher_v2(?,?);",
 		requestApplyVoucher.Code, requestApplyVoucher.SessionId)
 	if err.Error != nil {
 		return false, err.Error
